44b4f602ad549df8b3310e85e3687697c49037df
Rahman <articulatorrahman@gmail.com>
2026-05-24 00:16:17 +0530

    fix: add trustHost to NextAuth config

--- src/auth.ts
+++ src/auth.ts
@@ -5,6 +5,7 @@ import bcrypt from "bcryptjs";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   session: { strategy: "jwt" },
+  trustHost: true,
   pages: {
     signIn: "/login",
   },
